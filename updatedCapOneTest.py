166f4b8e0bc4a51d0557eaaa0b650e0d07a6fa23
pothyn <72640146+pothyn@users.noreply.github.com>
2020-10-10 14:17:42 -0400

    Create updatedCapOneTest.py

---
+++ updatedCapOneTest.py
@@ -0,0 +1,42 @@
+import requests
+import json
+
+customerId = 'Pothyn'
+apiKey = '2bd4021425dbdbbb6aafac992c0ad0a8'
+
+url = 'http://api.reimaginebanking.com/customers/{}/accounts'.format(customerId)
+# url = 'http://api.reimaginebanking.com/customers/{}/accounts?key={}'.format(customerId,apiKey)
+# url = 'http://api.reimaginebanking.com/customers?key={}'.format(apiKey)
+
+payload = {
+  "first_name": "Hunter",
+  "last_name": "Davis",
+  "address": {
+    "street_number": "99",
+    "street_name": "MLKJR",
+    "city": "Miami",
+    "state": "FL",
+    "zip": "22323"
+  }
+}
+
+customer = {
+  "code": 0,
+  "message": "string",
+  "fields": "string"
+}
+
+# Create a Savings Account
+response = requests.post( 
+	url, 
+	data=json.dumps(payload),
+	headers={'content-type':'application/json'},
+	)
+
+print(url)
+response = requests.post(url = url, data = payload)
+print(response.text)
+
+if response.status_code == 201:
+	print('account created')
+print('confirm')
