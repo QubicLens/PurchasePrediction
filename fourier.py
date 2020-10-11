758f1da3c9c738c18b72c7c639eb642bf5bc5dc4
Santiago Ro <srodvasquez@gmail.com>
2020-10-11 07:36:58 -0400

    Add Arbitrary Time Series Support

--- fourier.py
+++ fourier.py
@@ -7,51 +7,61 @@ TAU = 2 * np.pi
 PI2 = np.pi / 2
 
 
-def cat_coeff(x, u, k, s):
+# ----- CAT Fourier Analysis ------
+
+
+def cat_coeff(x, k, s):
     # Number of samples for integration
-    t = np.linspace(0, TAU, s)
+    t = np.linspace(0, TAU, s, endpoint=False)
 
     # Approximate Data Transform using Simpson's Rule
     rho = 1j * PI2 * np.floor((t[:, newaxis] @ k[newaxis, :]) / PI2)
-    A = simps(np.einsum("tj,tk->tjk", np.exp(-rho), np.exp(rho)), t, axis=0)
-    B = simps(np.einsum("t,tj->tj", x(t), np.exp(-rho)), t, axis=0)
+    A = simps(np.einsum("tj,tk->tjk", np.exp(-rho), np.exp(rho)), t, axis=0) / TAU
+    B = simps(np.einsum("t,tj->tj", x(t), np.exp(-rho)), t, axis=0) / TAU
 
     # Compute coefficients in Frequency Domain
-    c = [1, 1j, -1, -1j, 1][4 * u]
-    return np.linalg.solve(c * A, B)
+    return np.linalg.solve(A, B)
 
 
-def phi(k, t, u):
-    t = np.mod(t, TAU)  # Expand CAT function to that of a periodic function
-    return np.exp(1j * PI2 * np.floor((t[:, newaxis] @ k[newaxis, :]) / PI2 + 4 * u))
+def phi(k, t):
+    # Extend step function periodically
+    t = np.mod(t, TAU)
+    return np.exp(1j * PI2 * np.floor((t[:, newaxis] @ k[newaxis, :]) / PI2))
 
 
-def predict(x, t, k=23, s=1999):
-    u = 0  # TODO: calculating phase, time scaling
-
-    # Compute coefficients of 2k+1 terms in CAT Series
+def analyze(x, k=23, s=1999):
+    # Compute coefficients of 2k+1 terms in CAT Sequence
     k = np.arange(-k, k + 1)
-    Yk = cat_coeff(x, u, k, s)
+    Yk = cat_coeff(x, k, s)
 
-    # Compute category at each time t
-    return np.real(np.einsum("tk,k->t", phi(k, t, u), Yk))
+    # Predict category for any given time t
+    return lambda t: np.real(np.einsum("tk,k->t", phi(k, t), Yk))
+
+
+# ----- I/O Formatting ------
+
+
+def interpolate(arr):
+    # Convert discrete time series to a stepwise function
+    return lambda t: arr[np.floor(len(arr) * np.mod(t, TAU) / TAU).astype(int)]
+
+
+def interpret(qcat):
+    # Simplify into an unambiguous category prediction
+    return np.clip(np.around(qcat), 0, None).astype(int)
+
+
+# ---- DEBUG -----
 
 
 if __name__ == "__main__":
-    x0 = lambda t: np.piecewise(
-        t,
-        [
-            (0 <= t) * (t < PI2),
-            (PI2 <= t) * (t < np.pi),
-            (np.pi <= t) * (t < 3 * PI2),
-            (3 * PI2 <= t) * (t < TAU),
-        ],
-        [1, 0, 2, 0],
-    )
+    interval = 30
+    x = np.array([1 if _ % 10 == 0 else 0 for _ in range(interval)])
 
-    x = lambda t: x0(np.mod(t, TAU))
-    xp = predict(x, np.linspace(0, TAU, 18), 10, 19999)
+    x = interpolate(x)
+    xp = analyze(x, 20, 9999)
+    t = np.linspace(0, 2 * TAU, 2 * interval, endpoint=False)
 
-    print(f"Quasi-Category: {xp}\n")
-    print(f"Predicted: {np.maximum(0, np.around(xp))}\n")
-    print(f"Actual: {x(np.linspace(0, TAU, 18))}\n")
+    print(f"Quasi-Category: {xp(t)}\n")
+    print(f"Predicted: {interpret(xp(t))}\n")
+    print(f"Actual: {x(t)}\n")
